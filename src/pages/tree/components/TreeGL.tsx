06b97909148f181473324a6bcb8ce31044d53b9b
ShallowGreen <183339728@qq.com>
2022-07-26 10:46:43 +0800

    feat: add css2drender

--- src/pages/tree/components/TreeGL.tsx
+++ src/pages/tree/components/TreeGL.tsx
@@ -4,6 +4,8 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { getPublicAssetPath, IS_MOBILE } from '../../../utils';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 import { PageType } from '../../app/App.config';
 import {
     AppContext,
@@ -13,6 +15,29 @@ import {
 } from '../../app/App.utils';
 import './TreeGL.less';
 
+
+const POINT_DATA = [
+    {
+        position: new THREE.Vector3(1.6, 1.7, 0.3),
+        camera: new THREE.Vector3(8, 2, 0),
+        title: 'GameWorld1',
+    },
+    {
+        position: new THREE.Vector3(2.3, 0, 1.3),
+        camera: new THREE.Vector3(8, 2, 0),
+        title: 'Swap',
+    },
+    {
+        position: new THREE.Vector3(1.8, 0.3, -1.7),
+        camera: new THREE.Vector3(0, 2, -8),
+        title: 'Meritocracy',
+    },
+    {
+        position: new THREE.Vector3(-1, 0.2, 1.5),
+        camera: new THREE.Vector3(0, 2, 8),
+        title: 'GameMaster',
+    }
+]
 export const TreeGL = (props) => {
     const containerRef = useRef<HTMLDivElement>(null);
 
@@ -21,31 +46,65 @@ export const TreeGL = (props) => {
         if (!container) {
             return;
         }
-        const clock = new THREE.Clock();
+        let frameId: number = 0;
         let mixer: THREE.AnimationMixer;
-        let frameId: number;
-        let loaded = false;
-        let loading = false;
+        const clock = new THREE.Clock();
+        let isAutoRotate: boolean = true;
+        let root: THREE.Group;
+
+        // FPS
+        // const stats = new Stats();
+        // container.appendChild( stats.dom );
+
+        // 初始化render,背景透明
         const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.outputEncoding = THREE.sRGBEncoding;
         container.appendChild(renderer.domElement);
+
+        const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
         // 初始化场景
         const scene = new THREE.Scene();
+        // scene.background = new THREE.Color( 0xbfe3dd );
+        scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture;
 
-        const camera = new THREE.PerspectiveCamera(40, 1, 1, 100);
+        root = new THREE.Group();
+        scene.add(root);
+
+        const labelRenderer = new CSS2DRenderer();
+        labelRenderer.setSize(container.clientWidth, container.clientHeight);
+        // labelRenderer.domElement.style.position = 'absolute';
+        // labelRenderer.domElement.style.top = '0px';
+        labelRenderer.domElement.style.pointerEvents = 'none';
+        labelRenderer.domElement.className = 'vision-canvas';
+        container.appendChild(labelRenderer.domElement);
+
+        // 初始化相机
+        const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 100);
         camera.position.set(5, 2, 8);
-        camera.lookAt(0, 0, 0);
-        const controls = new OrbitControls(camera, container);
-        controls.update();
+
+
+        // 初始化控制器
+        const controls = new OrbitControls(camera, renderer.domElement);
+        //设置控制器的中心点
+        controls.target.set(0, 0.5, 0);
+
         controls.enablePan = false;
+        // 使动画循环使用时阻尼或自转 意思是否有惯性
         controls.enableDamping = true;
+        //动态阻尼系数 就是鼠标拖拽旋转灵敏度
+        // 阻尼系数
+        controls.dampingFactor = 0.1;
+        //是否可以缩放
         controls.enableZoom = false;
         controls.minPolarAngle = Math.PI * 0.5;
         controls.maxPolarAngle = Math.PI * 0.5;
+        //是否自动旋转
         controls.autoRotate = true;
         controls.autoRotateSpeed = 1.5;
+        let model: THREE.Group;
 
         const loader = new GLTFLoader();
         loader.load(
@@ -55,7 +114,11 @@ export const TreeGL = (props) => {
                 model.scale.set(1, 1, 1);
                 scene.add(model);
                 mixer = new THREE.AnimationMixer(model);
-                loaded = true;
+
+                mixer = new THREE.AnimationMixer(model);
+                mixer.clipAction(gltf.animations[0]).play();
+
+                animate();
                 loadingEE.emit(
                     `progress.${LoadingSourceType.TREE_MODEL}`,
                     1
@@ -81,14 +144,17 @@ export const TreeGL = (props) => {
 
         // }
 
-        function animate() {
-            frameId = requestAnimationFrame(animate);
-            const delta = clock.getDelta();
+        POINT_DATA.forEach((item, index) => {
+            const div = document.createElement('div');
+            div.className = 'vision-label';
+            div.innerHTML = `<div class="vision-label-text">${item.title}</div>`
+            // div.textContent = 'test'
+            const label = new CSS2DObject(div);
+            label.position.set(item.position.x, item.position.y, item.position.z);
+            root.add(label);
 
-            mixer.update(delta);
-            renderer.render(scene, camera);
-            controls.update();
-        }
+        })
+
 
         window.onresize = function () {
 
@@ -96,8 +162,25 @@ export const TreeGL = (props) => {
             camera.updateProjectionMatrix();
 
             renderer.setSize(window.innerWidth, window.innerHeight);
+            labelRenderer.setSize(window.innerWidth, window.innerHeight);
 
         };
+
+        function animate() {
+            frameId = requestAnimationFrame(animate);
+
+            const delta = clock.getDelta();
+
+            mixer.update(delta);
+
+            controls.update();
+
+            // stats.update();
+
+            renderer.render(scene, camera);
+            labelRenderer.render(scene, camera);
+
+        }
 
         /** 首页loading结束后，再开始loading */
         // loadingEE.on('loaded', () => setTimeout(load, 200));
